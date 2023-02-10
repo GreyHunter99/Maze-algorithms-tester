b21bdfc35e940c8d4a761d287d74259346a8f9f6
Berni <bernard.swiatek@gmail.com>
2023-02-10 21:40:57 +0100

    first AI, visualisation cleanup and functionality

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from maze import *
 app = Flask(__name__)
+
+last_maze = Maze(10)
 
 
 @app.route('/')
 def index():
     """ Main page """
-    maze = Maze(10)
-    return render_template('index.html', maze=maze)
+    return redirect(url_for("visualisation"))
+    # return render_template('index.html')
+
+
+@app.route('/visualisation')
+def visualisation():
+    """ Algorithm visualisation """
+    global last_maze
+
+    return render_template('visualisation.html', maze=last_maze)
+
+
+@app.route('/generate')
+def generate():
+    """ Generate maze """
+    global last_maze
+
+    last_maze = Maze(10)
+    return redirect(url_for("visualisation"))
+
+
+@app.route('/solve')
+def solve():
+    """ Solve maze """
+    global last_maze
+
+    last_maze.ai()
+    return redirect(url_for("visualisation"))
+
+
+@app.route('/clear')
+def clear():
+    """ Clear maze """
+    global last_maze
+
+    last_maze.clear()
+    return redirect(url_for("visualisation"))
 
 
 if __name__ == '__main__':
--- maze.py
+++ maze.py
@@ -6,7 +6,9 @@ class Maze:
     """ Maze class """
     def __init__(self, size):
         self.size = size
-        self.end = ()
+        self.end = []
+        self.spawn = []
+        self.moves = 0
         self.cells = self.generate_cells()
 
     def generate_cells(self):
@@ -58,4 +60,40 @@ class Maze:
                 else:
                     break
 
+        for x in range(int(self.size / 2 - self.size / 10), int(self.size / 2 + self.size / 10)):
+            for y in range(int(self.size / 2 - self.size / 10), int(self.size / 2 + self.size / 10)):
+                self.spawn.append((x, y))
+        self.spawn = random.choice(self.spawn)
+
+        for x in range(self.size):
+            for y in range(self.size):
+                cells[x][y].visited = 0
+
         return cells
+
+    def ai(self):
+        for x in range(self.size):
+            for y in range(self.size):
+                self.cells[x][y].visited = 0
+
+        position = self.cells[self.spawn[0]][self.spawn[1]]
+        position.visited += 1
+        self.moves = 0
+        while position != self.cells[self.end[0]][self.end[1]]:
+            neighbours = []
+            if not position.walls['top']:
+                neighbours.append(self.cells[position.x][position.y + 1])
+            if not position.walls['bottom']:
+                neighbours.append(self.cells[position.x][position.y - 1])
+            if not position.walls['left']:
+                neighbours.append(self.cells[position.x - 1][position.y])
+            if not position.walls['right']:
+                neighbours.append(self.cells[position.x + 1][position.y])
+            position = random.choice(neighbours)
+            position.visited += 1
+            self.moves += 1
+
+    def clear(self):
+        for x in range(self.size):
+            for y in range(self.size):
+                self.cells[x][y].visited = 0
